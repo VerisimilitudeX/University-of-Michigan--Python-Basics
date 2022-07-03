dc5b371f2daa6d6ba50dc098379dd0565981f403
Piyush Acharya <acharyapiyush1@outlook.com>
2022-07-02 19:13:22 -0700

    Create Print out each element in a list and its type.py

---
+++ Print out each element in a list and its type.py
@@ -0,0 +1,6 @@
+
+several_things = ["hello", 2, 4, 6.0, 7.5, 234352354, "the end", "", 99]
+for element in several_things:
+    print(element)
+for element in several_things:
+    print(type(element))
