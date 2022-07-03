eaaf6009292a7f198abe8dfc0c643157463d7e03
Piyush Acharya <acharyapiyush1@outlook.com>
2022-07-02 20:40:32 -0700

    Create Convert present tense to past tense.py

---
+++ Convert present tense to past tense.py
@@ -0,0 +1,7 @@
+
+wrds = ["end", 'work', "play", "start", "walk", "look", "open", "rain", "learn", "clean"]
+past_wrds = []
+for word in wrds:
+    word = word + "ed"
+    past_wrds.append(word)
+print(past_wrds)
